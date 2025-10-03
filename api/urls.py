75404fc3531411dbfa0cde2400db53d0cf2cedf8
zhman <imdadullahzhman11@gmail.com>
2025-10-03 09:00:36 +0430

    url added for teachview

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
 
-from .views import TestView
+from .views import TestView,TeachView
 
 urlpatterns = [
     path('TestView', Views.TestView),
+    path('teachview', Views.TeachView),
 ]
